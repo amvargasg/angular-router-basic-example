89b491f942ac622ef308758bf453311974414cf8
amvargasg <am.vargasg@uniandes.edu.co>
2020-07-30 13:40:28 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-router-basic-example-x3yixq
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-router-basic-example-x3yixq)
---
+++ app/views/catalog/catalog.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { Http } from '@angular/http';
+
+@Component({
+  selector: 'home',
+  template: `
+    <h1>Catalog asdjfhasdjf</h1>
+  `,
+})
+export class CatalogViewComponent {
+}
